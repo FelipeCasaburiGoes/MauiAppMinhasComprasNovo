9999b14261f5b7d6d8e088976c5502fc45b4f33c
FelipeCasaburiGoes <casaburifelipe@gmail.com>
2025-03-07 11:02:42 -0300

    Atualizado 07 de Março de 2025

--- MauiAppMinhasComprasNovo/App.xaml.cs
+++ MauiAppMinhasComprasNovo/App.xaml.cs
@@ -1,12 +1,34 @@
-﻿namespace MauiAppMinhasComprasNovo
+﻿using MauiAppMinhasComprasNovo.Helpers;
+
+namespace MauiAppMinhasComprasNovo  
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            //MainPage = new AppShell();
+           
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
--- MauiAppMinhasComprasNovo/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasComprasNovo/Helpers/SQLiteDatabaseHelper.cs
@@ -1,41 +1,42 @@
-﻿using MauiAppMinhasComprasNovo.Models;
-using SQLite;
+﻿//Using são como estiquetas que identificam as ferramentas
+using MauiAppMinhasComprasNovo.Models; //pois usaremos a pasta models
+using SQLite; //vem do NuGet
 
 namespace MauiAppMinhasComprasNovo.Helpers
 {
     public class SQLiteDatabaseHelper
     {
-        readonly SQLiteAsyncConnection _conn;
+        readonly SQLiteAsyncConnection _conn; //só para leitura, é assincrona e faz a conexão com o sqlite 
 
-        public SQLiteDatabaseHelper(string path)
+        public SQLiteDatabaseHelper(string path)//é o construtor da classe, é o caminho para o arquivo
         {
-            _conn = new SQLiteAsyncConnection(path);
-            _conn.CreateTableAsync<Produto>().Wait();
+            _conn = new SQLiteAsyncConnection(path);//conexão com o caminho
+            _conn.CreateTableAsync<Produto>().Wait();//cria a tabela produto e espera a tarefa se concluir
         }
 
-        public Task<int> Insert(Produto p) 
+        public Task<int> Insert(Produto p) //insere o produto na tabela
         {
-            return _conn.InsertAsync(p);
+            return _conn.InsertAsync(p);//retorna uma tarefa no arquivo de texto, de forma assíncrona
         }
 
-        public Task<List<Produto>> Update(Produto p)
+        public Task<List<Produto>> Update(Produto p)//atualizar
         {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";//é a diretiva sql
 
-            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);//qual vai ser a tabela
         }
 
-        public Task <int> Delete(int id)
+        public Task <int> Delete(int id)//deletar
         {
-            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id); 
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);//para saber quantos produtos foram excluídos na operação
         }
 
-        public Task<List<Produto>> GetAll()
+        public Task<List<Produto>> GetAll()//listar todos os produtos
         {
-            return _conn.Table<Produto>().ToListAsync();
+            return _conn.Table<Produto>().ToListAsync();//retorna uma array de objetos
         }
 
-        public Task<List<Produto>> Search(string q) 
+        public Task<List<Produto>> Search(string q) //pesquisar no banco de dados, pesquisa instantânea
         {
             string sql = "SELECT + Produto WHERE descricao LIKE '%" + q + "%'";
 
